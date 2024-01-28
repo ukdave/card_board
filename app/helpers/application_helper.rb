d7f6381b52c4016e0afa22550517230a0eac5d3f
David Bull <david@uk-dave.com>
2024-01-28 22:35:17 +0000

    Add timeago stimulus controller

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+  def timeago(date, format: :long, refresh_interval_millis: 5000, add_suffix: true)
+    return if date.blank?
+
+    content = I18n.l(date, format: format)
+
+    tag.time(content,
+      title: content,
+      data: {
+        controller: "timeago",
+        timeago_datetime_value: date.iso8601,
+        timeago_refresh_interval_value: refresh_interval_millis,
+        timeago_add_suffix_value: add_suffix ? "" : nil
+      }.compact)
+  end
 end
--- config/importmap.rb
+++ config/importmap.rb
@@ -8,4 +8,5 @@ pin "bootstrap", to: "https://ga.jspm.io/npm:bootstrap@5.3.2/dist/js/bootstrap.e
 pin "@popperjs/core", to: "https://ga.jspm.io/npm:@popperjs/core@2.11.8/lib/index.js"
 pin "sortablejs", to: "https://ga.jspm.io/npm:sortablejs@1.15.0/modular/sortable.esm.js"
 pin "@rails/request.js", to: "https://ga.jspm.io/npm:@rails/request.js@0.0.9/src/index.js"
+pin "date-fns", to: "https://ga.jspm.io/npm:date-fns@3.3.1/index.mjs"
 pin_all_from "app/javascript/controllers", under: "controllers"
