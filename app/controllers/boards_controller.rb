829bc0432fde378ebe2aeb2786b80c3571e4dab8
David Bull <david@uk-dave.com>
2024-01-28 22:44:19 +0000

    Add actions to create, update, and delete boards

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,9 +1,35 @@
 class BoardsController < ApplicationController
+  before_action :set_project, only: %i[show update destroy]
+
   def index
     @boards = Board.all
   end
 
   def show
-    @board = Board.find_by(id: params[:id])
+  end
+
+  def create
+    @board = Board.create!(board_params)
+    redirect_to board_path(@board), notice: "Board was successfully created."
+  end
+
+  def update
+    @board.update!(board_params)
+    redirect_to board_path(@board), notice: "Board was successfully updated."
+  end
+
+  def destroy
+    @board.destroy!
+    redirect_to boards_path, notice: "Board was successfully deleted."
+  end
+
+  private
+
+  def set_project
+    @board = Board.find(params[:id])
+  end
+
+  def board_params
+    params.require(:board).permit(:name)
   end
 end
---
+++ spec/requests/boards_spec.rb
@@ -0,0 +1,93 @@
+require "rails_helper"
+
+describe "Boards" do
+  describe "GET /boards" do
+    before do
+      Board.create!(name: "Some Board")
+      Board.create!(name: "Another Board")
+      get "/boards"
+    end
+
+    it "returns a list of boards" do
+      expect(response.body).to include("Some Board", "Another Board")
+    end
+
+    it "returns success" do
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "POST /boards" do
+    before do
+      post "/boards", params: {board: {name: "Test Board"}}
+    end
+
+    it "creates a board" do
+      expect(Board.last).to have_attributes({name: "Test Board"})
+    end
+
+    it "redirects to the board" do
+      expect(response).to redirect_to(Board.last)
+    end
+
+    it "sets a success flash message" do
+      expect(flash[:notice]).to eq "Board was successfully created."
+    end
+  end
+
+  describe "GET /board/{id}" do
+    let(:board) { Board.create!(name: "Test Board") }
+
+    before do
+      get "/boards/#{board.id}"
+    end
+
+    it "incldues the name of the board" do
+      expect(response.body).to include("Test Board")
+    end
+
+    it "returns success" do
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "PUT /board/{id}" do
+    let(:board) { Board.create!(name: "Test Board") }
+
+    before do
+      put "/boards/#{board.id}", params: {board: {name: "Some New Name"}}
+    end
+
+    it "updates the name of the board" do
+      expect(board.reload.name).to eq("Some New Name")
+    end
+
+    it "redirects to the board" do
+      expect(response).to redirect_to(board)
+    end
+
+    it "sets a success flash message" do
+      expect(flash[:notice]).to eq "Board was successfully updated."
+    end
+  end
+
+  describe "DELETE /board/{id}" do
+    let(:board) { Board.create!(name: "Test Board") }
+
+    before do
+      delete "/boards/#{board.id}"
+    end
+
+    it "deletes the board" do
+      expect(Board.exists?(id: board.id)).to be false
+    end
+
+    it "redirects to the boards index page" do
+      expect(response).to redirect_to(Board)
+    end
+
+    it "sets a success flash message" do
+      expect(flash[:notice]).to eq "Board was successfully deleted."
+    end
+  end
+end
